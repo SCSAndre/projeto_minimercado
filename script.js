ab07db1b24a7475f1a1887b458af9ec85c260fde
unknown <safarcs@gmail.com>
2025-09-04 15:05:47 -0300

    Criação e alterações necessárias

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     // --- LÓGICA DAS NOTIFICAÇÕES (TOAST) ---
-    // 1. Seleciona o elemento Toast do HTML.
     const toastElement = document.getElementById('notificacaoToast');
-    // 2. Cria um objeto Toast do Bootstrap, que podemos controlar com JS.
     const toast = new bootstrap.Toast(toastElement);
     
     // --- MANIPULAÇÃO DO FORMULÁRIO DE CADASTRO ---
@@ -13,7 +11,6 @@ document.addEventListener('DOMContentLoaded', function() {
         formCadastro.addEventListener('submit', function(event) {
             event.preventDefault(); 
             
-            // Define a mensagem do toast e o exibe
             const toastBody = toastElement.querySelector('.toast-body');
             toastBody.textContent = 'Cadastro realizado com sucesso! Em breve você receberá nossas novidades.';
             toast.show();
@@ -32,7 +29,6 @@ document.addEventListener('DOMContentLoaded', function() {
             const dataAgendada = document.getElementById('inputData').value;
             const horaAgendada = document.getElementById('inputHorario').value;
 
-            // Define a mensagem personalizada do toast e o exibe
             const toastBody = toastElement.querySelector('.toast-body');
             toastBody.textContent = `Agendamento confirmado para o dia ${dataAgendada} às ${horaAgendada}. Obrigado!`;
             toast.show();
